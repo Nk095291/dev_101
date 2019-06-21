4aa8122f5d6971dcabe95f29f7a17e98686fed18
NK095291 <nk095291@gmail.com>
2019-06-21 21:52:32 +0530

    21 june 2019

--- nodejs_and_js/class11/readline.js
+++ nodejs_and_js/class11/readline.js
@@ -1,31 +1,50 @@
 const readline = require("readline");
+const yargs = require("yargs");
+const chalk = require("chalk");
+const figlet = require("figlet");
 
 
 const commands = require("./commands.js");
 
+// const color =
+
 const reader = readline.createInterface({
     input:process.stdin,
     output:process.stdout,
-    prompt:"<nitin's>"
+    prompt:chalk.bold.bgBlueBright.inverse(figlet.textSync('hey bade',{font:"Ghost",horizontalLayout:"full",verticalLayout:"full"}))
 });
 
 reader.prompt();
 reader.on("line",function(data){
+    console.log(yargs.argv);
     data = data.split(' ');
-    if(commands.hasOwnProperty(data[0])===true)
-    {
-        commands[data[1]];
+    data.argv._=data;
+    console.log(yargs.argv._);
+    
+    if(data[0]==='ls'){
+        commands.ls(data[1]==undefined?'./':data[1]);
     }
+    else if(data[0]==='mkdir')
+    {
+        commands.mkdir(data[1]);
+    }
+    else if(data[0]==='rmdir')
+    {
+        commands.rmdir(data[1]);
+    }
+    else if(data[0]==='touch')
+    {
+        commands.touch(data[1]);
+    }
+    else if(data[0]==='rm')
+    {
+        commands.rm(data[1]);
+    }
+
+
     reader.prompt();
 })
 reader.on("close",function(){
     console.log();
     console.log("see yeah");
 })
-
-
-// commands.ls("./");
-// commands.mkdir('./yo');
-// commands.rmdir('./yo');
-// commands.touch('yo.txt')
-// commands.rm('yo.txt')
